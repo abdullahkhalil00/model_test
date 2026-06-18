95dc39784132a28d7280d2c4db2a49e7fa685c81
Abdullah Khalil <153406500+abdullahkhalil00@users.noreply.github.com>
2026-06-18 15:00:26 +0500

    a

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from huggingface_hub import InferenceClient
 
-st.title("Multi Model AI")
+st.title("Multi Model AI updated")
 
 client = InferenceClient(api_key=st.secrets["HF_TOKEN"])
 
